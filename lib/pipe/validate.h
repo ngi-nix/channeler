d1d48a28bc143c148d064566732b5293ce95c8a3
Jens Finkhaeuser <jens@finkhaeuser.de>
2021-02-23 10:51:21 +0100

    Add and test validation filter, which produces filter request actions based on policy parameters

---
+++ lib/pipe/validate.h
@@ -0,0 +1,178 @@
+/**
+ * This file is part of channeler.
+ *
+ * Author(s): Jens Finkhaeuser <jens@finkhaeuser.de>
+ *
+ * Copyright (c) 2021 Jens Finkhaeuser.
+ *
+ * This software is licensed under the terms of the GNU GPLv3 for personal,
+ * educational and non-profit use. For all other uses, alternative license
+ * options are available. Please contact the copyright holder for additional
+ * information, stating your intended usage.
+ *
+ * You can find the full text of the GPLv3 in the COPYING file in this code
+ * distribution.
+ *
+ * This software is distributed on an "AS IS" BASIS, WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.
+ **/
+#ifndef CHANNELER_PIPE_VALIDATE_H
+#define CHANNELER_PIPE_VALIDATE_H
+
+#ifndef __cplusplus
+#error You are trying to include a C++ only header file
+#endif
+
+#include <channeler.h>
+
+#include <memory>
+#include <set>
+
+#include "../memory/packet_pool.h"
+#include "event.h"
+#include "action.h"
+
+#include <channeler/packet.h>
+#include <channeler/error.h>
+
+
+namespace channeler::pipe {
+
+/**
+ * Null policy - does nothing, but provides the right kind of interface.
+ *
+ * See validate_filter for details.
+ */
+template <
+  typename addressT = peerid
+>
+struct null_policy
+{
+  // FIXME ingress as an enum? see action.h
+  inline bool should_filter(addressT const &, bool ingress [[maybe_unused]])
+  {
+    return false;
+  }
+};
+
+
+
+/**
+ * Validate packets.
+ *
+ * On the face of it, this filter merely validates packet checksums or similar
+ * cryptographic mechanisms in order to decide whether a packet should be
+ * processed further.
+ *
+ * But beyond that, this is also the place in which it should be decided
+ * what consequences receiving an invalid packet has.
+ *
+ * If a packet is known to be invalid, the filter consults the peer failure
+ * policy to decide whether to institute filtering at the level of the peerid,
+ * and the transport failure policy whether to filter at the transport level.
+ */
+template <
+  typename addressT,
+  std::size_t POOL_BLOCK_SIZE,
+  typename next_filterT,
+  typename next_eventT,
+  typename peer_failure_policyT = null_policy<peerid_wrapper>,
+  typename transport_failure_policyT = null_policy<addressT>
+>
+struct validate_filter
+{
+  using pool_type = ::channeler::memory::packet_pool<POOL_BLOCK_SIZE>;
+  using slot_type = typename pool_type::slot;
+
+  struct input_event : public event
+  {
+    addressT                  src_addr;
+    addressT                  dst_addr;
+    channeler::packet_wrapper packet;
+    slot_type                 data;
+
+    inline input_event(addressT const & src, addressT const & dst,
+        channeler::packet_wrapper const & pkt,
+        slot_type const & slot)
+      : event{ET_DECRYPTED_PACKET}
+      , src_addr{src}
+      , dst_addr{dst}
+      , packet{pkt}
+      , data{slot}
+    {
+    }
+  };
+
+
+  inline validate_filter(next_filterT * next,
+      peer_failure_policyT * peer_p = nullptr,
+      transport_failure_policyT * trans_p = nullptr)
+    : m_next{next}
+    , m_peer_policy{peer_p}
+    , m_transport_policy{trans_p}
+  {
+  }
+
+
+  inline action_list_type consume(std::unique_ptr<event> ev)
+  {
+    if (!ev) {
+      throw exception{ERR_INVALID_REFERENCE};
+    }
+    if (ev->type != ET_DECRYPTED_PACKET) {
+      throw exception{ERR_INVALID_PIPE_EVENT};
+    }
+
+    input_event * in = reinterpret_cast<input_event *>(ev.get());
+
+    // If there is no data passed, we should also throw.
+    if (nullptr == in->data.data()) {
+      throw exception{ERR_INVALID_REFERENCE};
+    }
+
+    // We need to validate the packet. For now, this just means verifying the
+    // checksum.
+    if (!in->packet.has_valid_checksum()) {
+      action_list_type res;
+      if (m_peer_policy) {
+        if (m_peer_policy->should_filter(in->packet.sender(), true)) {
+          res.push_back(std::make_shared<peer_filter_request_action>(
+                  in->packet.sender(), true));
+        }
+        if (m_peer_policy->should_filter(in->packet.recipient(), false)) {
+          res.push_back(std::make_shared<peer_filter_request_action>(
+                  in->packet.recipient(), false));
+        }
+      }
+
+      if (m_transport_policy) {
+        if (m_transport_policy->should_filter(in->src_addr, true)) {
+          res.push_back(std::make_shared<transport_filter_request_action<addressT>>(
+                in->src_addr, true));
+        }
+        if (m_transport_policy->should_filter(in->dst_addr, false)) {
+          res.push_back(std::make_shared<transport_filter_request_action<addressT>>(
+                in->dst_addr, false));
+        }
+      }
+
+      // If the checksum is invalid, we know we want to exit the pipe here.
+      return res;
+    }
+
+    // At the next filter, we require full packets again. Let's just move
+    // the event, then.
+    return m_next->consume(std::move(ev));
+  }
+
+
+  next_filterT *              m_next;
+  peer_failure_policyT *      m_peer_policy;
+  transport_failure_policyT * m_transport_policy;
+};
+
+
+} // namespace channeler::pipe
+
+#endif // guard
---
+++ test/private/pipe/validate.cpp
@@ -0,0 +1,387 @@
+/**
+ * This file is part of channeler.
+ *
+ * Author(s): Jens Finkhaeuser <jens@finkhaeuser.de>
+ *
+ * Copyright (c) 2021 Jens Finkhaeuser.
+ *
+ * This software is licensed under the terms of the GNU GPLv3 for personal,
+ * educational and non-profit use. For all other uses, alternative license
+ * options are available. Please contact the copyright holder for additional
+ * information, stating your intended usage.
+ *
+ * You can find the full text of the GPLv3 in the COPYING file in this code
+ * distribution.
+ *
+ * This software is distributed on an "AS IS" BASIS, WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.
+ **/
+
+#include "../lib/pipe/validate.h"
+
+#include <gtest/gtest.h>
+
+namespace {
+
+
+inline constexpr std::byte operator "" _b(unsigned long long arg) noexcept
+{
+  return static_cast<std::byte>(arg);
+}
+
+
+std::byte const packet01[] = {
+  // **** public header
+  // Proto
+  0xde_b, 0xad_b, 0xd0_b, 0x0d_b,
+
+  // Sender
+  0x00_b, 0x00_b, 0x00_b, 0x00_b,  0x00_b, 0x00_b, 0x00_b, 0x00_b,
+  0x00_b, 0x00_b, 0x00_b, 0x00_b,  0x00_b, 0x0a_b, 0x11_b, 0xc3_b,
+
+  // Recipient
+  0x00_b, 0x00_b, 0x00_b, 0x00_b,  0x00_b, 0x00_b, 0x00_b, 0x00_b,
+  0x00_b, 0x00_b, 0x00_b, 0x00_b,  0x00_b, 0x00_b, 0x0b_b, 0x0b_b,
+
+  // Channel identifier - all zeroes is the default channel
+  0x00_b, 0x00_b, 0x00_b, 0x00_b,
+
+  // Flags
+  0xa0_b, 0x0a_b,
+
+  // Packet size - packet is empty, this includes the envelope
+  0x00_b, 0x34_b,
+
+  // **** private header
+  // Sequence number - a random one is fine
+  0x01_b, 0xfa_b,
+
+  // Payload size - no payload
+  0x00_b, 0x00_b,
+ 
+  // **** payload
+  // n/a
+
+  // **** footer
+  // Checksum
+  0xfa_b, 0xfa_b, 0x25_b, 0xc3_b,
+};
+
+
+
+// For testing
+using address_t = uint16_t;
+constexpr std::size_t POOL_BLOCK_SIZE = 3;
+constexpr std::size_t PACKET_SIZE = sizeof(packet01);
+
+using pool_type = ::channeler::memory::packet_pool<POOL_BLOCK_SIZE>;
+
+struct next
+{
+  struct input_event : public channeler::pipe::event
+  {
+    inline input_event(address_t src, address_t dst, channeler::packet_wrapper p,
+        pool_type::slot slot)
+      : event{channeler::pipe::ET_DECRYPTED_PACKET}
+      , src_addr{src}
+      , dst_addr{dst}
+      , packet{p}
+      , data{slot}
+    {
+    }
+
+    address_t                 src_addr;
+    address_t                 dst_addr;
+    channeler::packet_wrapper packet;
+    pool_type::slot           data;
+  };
+
+  inline channeler::pipe::action_list_type consume(std::unique_ptr<channeler::pipe::event> event)
+  {
+    m_event = std::move(event);
+    return {};
+  }
+
+  std::unique_ptr<channeler::pipe::event> m_event = {};
+};
+
+using simple_filter_t = channeler::pipe::validate_filter<
+  address_t,
+  POOL_BLOCK_SIZE,
+  next,
+  next::input_event
+>;
+
+
+// Simple policy - rejects sender or recipient
+template <
+  typename addressT
+>
+struct reject_policy
+{
+  bool for_ingress;
+
+  inline bool should_filter(addressT const &, bool ingress)
+  {
+    return (ingress == for_ingress);
+  }
+};
+
+} // anonymous namespace
+
+
+
+TEST(ValidateFilter, throw_on_invalid_event)
+{
+  using namespace channeler::pipe;
+
+  next n;
+  simple_filter_t filter{&n};
+
+  // Create a default event; this should not be handled.
+  auto ev = std::make_unique<event>();
+  ASSERT_THROW(filter.consume(std::move(ev)), ::channeler::exception);
+
+  // The filter should also throw on a null event
+  ASSERT_THROW(filter.consume(nullptr), ::channeler::exception);
+}
+
+
+
+TEST(ValidateFilter, pass_packet)
+{
+  using namespace channeler::pipe;
+
+  pool_type pool{PACKET_SIZE};
+
+  // Copy packet data before parsing header
+  auto data = pool.allocate();
+  ::memcpy(data.data(), packet01, sizeof(packet01));
+  channeler::packet_wrapper packet{data.data(), data.size()};
+
+  next n;
+  simple_filter_t filter{&n};
+
+  // No data added to event.
+  auto ev = std::make_unique<simple_filter_t::input_event>(123, 321, packet, data);
+  ASSERT_NO_THROW(filter.consume(std::move(ev)));
+
+  // We expect the event to be passed on verbatim, so we'll test what there
+  // is in the output event.
+  ASSERT_EQ(n.m_event->type, ET_DECRYPTED_PACKET);
+  next::input_event * ptr = reinterpret_cast<next::input_event *>(n.m_event.get());
+  ASSERT_EQ(123, ptr->src_addr);
+  ASSERT_EQ(321, ptr->dst_addr);
+  ASSERT_EQ(ptr->packet.sender().display(), "0x000000000000000000000000000a11c3");
+  ASSERT_EQ(ptr->packet.recipient().display(), "0x00000000000000000000000000000b0b");
+}
+
+
+
+TEST(ValidateFilter, drop_packet)
+{
+  using namespace channeler::pipe;
+
+  pool_type pool{PACKET_SIZE};
+
+  // Copy packet data before parsing header
+  auto data = pool.allocate();
+  ::memcpy(data.data(), packet01, sizeof(packet01));
+  data.data()[sizeof(packet01) - 1] = 0x00_b; // Bad checksum - null the last byte
+  channeler::packet_wrapper packet{data.data(), data.size()};
+
+  next n;
+  simple_filter_t filter{&n};
+
+  // No data added to event.
+  auto ev = std::make_unique<simple_filter_t::input_event>(123, 321, packet, data);
+  ASSERT_NO_THROW(filter.consume(std::move(ev)));
+
+  // We expect the event not to be passed on.
+  ASSERT_FALSE(n.m_event);
+}
+
+
+
+TEST(ValidateFilter, drop_packet_ban_transport_source)
+{
+  using namespace channeler::pipe;
+
+  using peer_policy_t = reject_policy<channeler::peerid_wrapper>;
+  using transport_policy_t = reject_policy<address_t>;
+  using test_filter_t = channeler::pipe::validate_filter<
+    address_t,
+    POOL_BLOCK_SIZE,
+    next,
+    next::input_event,
+    peer_policy_t,
+    transport_policy_t
+  >;
+
+  pool_type pool{PACKET_SIZE};
+
+  // Copy packet data before parsing header
+  auto data = pool.allocate();
+  ::memcpy(data.data(), packet01, sizeof(packet01));
+  data.data()[sizeof(packet01) - 1] = 0x00_b; // Bad checksum - null the last byte
+  channeler::packet_wrapper packet{data.data(), data.size()};
+
+  next n;
+  transport_policy_t t{true};
+  test_filter_t filter{&n, nullptr, &t};
+
+  // No data added to event.
+  auto ev = std::make_unique<simple_filter_t::input_event>(123, 321, packet, data);
+  auto res = filter.consume(std::move(ev));
+
+  // We expect the event not to be passed on.
+  EXPECT_FALSE(n.m_event);
+
+  // We also expect the result list to contain a recommendation to filter the
+  // source address at the transport level
+  ASSERT_EQ(1, res.size());
+  auto action = *(res.begin());
+  ASSERT_EQ(action->type, AT_FILTER_TRANSPORT);
+  auto * ptr = reinterpret_cast<transport_filter_request_action<address_t> *>(action.get());
+  ASSERT_EQ(ptr->address, 123);
+  ASSERT_TRUE(ptr->ingress);
+}
+
+
+
+TEST(ValidateFilter, drop_packet_ban_transport_destination)
+{
+  using namespace channeler::pipe;
+
+  using peer_policy_t = reject_policy<channeler::peerid_wrapper>;
+  using transport_policy_t = reject_policy<address_t>;
+  using test_filter_t = channeler::pipe::validate_filter<
+    address_t,
+    POOL_BLOCK_SIZE,
+    next,
+    next::input_event,
+    peer_policy_t,
+    transport_policy_t
+  >;
+
+  pool_type pool{PACKET_SIZE};
+
+  // Copy packet data before parsing header
+  auto data = pool.allocate();
+  ::memcpy(data.data(), packet01, sizeof(packet01));
+  data.data()[sizeof(packet01) - 1] = 0x00_b; // Bad checksum - null the last byte
+  channeler::packet_wrapper packet{data.data(), data.size()};
+
+  next n;
+  transport_policy_t t{false};
+  test_filter_t filter{&n, nullptr, &t};
+
+  // No data added to event.
+  auto ev = std::make_unique<simple_filter_t::input_event>(123, 321, packet, data);
+  auto res = filter.consume(std::move(ev));
+
+  // We expect the event not to be passed on.
+  EXPECT_FALSE(n.m_event);
+
+  // We also expect the result list to contain a recommendation to filter the
+  // source address at the transport level
+  ASSERT_EQ(1, res.size());
+  auto action = *(res.begin());
+  ASSERT_EQ(action->type, AT_FILTER_TRANSPORT);
+  auto * ptr = reinterpret_cast<transport_filter_request_action<address_t> *>(action.get());
+  ASSERT_EQ(ptr->address, 321);
+  ASSERT_FALSE(ptr->ingress);
+}
+
+
+
+TEST(ValidateFilter, drop_packet_ban_peer_sender)
+{
+  using namespace channeler::pipe;
+
+  using peer_policy_t = reject_policy<channeler::peerid_wrapper>;
+  using transport_policy_t = reject_policy<address_t>;
+  using test_filter_t = channeler::pipe::validate_filter<
+    address_t,
+    POOL_BLOCK_SIZE,
+    next,
+    next::input_event,
+    peer_policy_t,
+    transport_policy_t
+  >;
+
+  pool_type pool{PACKET_SIZE};
+
+  // Copy packet data before parsing header
+  auto data = pool.allocate();
+  ::memcpy(data.data(), packet01, sizeof(packet01));
+  data.data()[sizeof(packet01) - 1] = 0x00_b; // Bad checksum - null the last byte
+  channeler::packet_wrapper packet{data.data(), data.size()};
+
+  next n;
+  peer_policy_t t{true};
+  test_filter_t filter{&n, &t};
+
+  // No data added to event.
+  auto ev = std::make_unique<simple_filter_t::input_event>(123, 321, packet, data);
+  auto res = filter.consume(std::move(ev));
+
+  // We expect the event not to be passed on.
+  EXPECT_FALSE(n.m_event);
+
+  // We also expect the result list to contain a recommendation to filter the
+  // source address at the transport level
+  ASSERT_EQ(1, res.size());
+  auto action = *(res.begin());
+  ASSERT_EQ(action->type, AT_FILTER_PEER);
+  auto * ptr = reinterpret_cast<peer_filter_request_action *>(action.get());
+  ASSERT_EQ(ptr->peer, packet.sender());
+  ASSERT_TRUE(ptr->ingress);
+}
+
+
+
+TEST(ValidateFilter, drop_packet_ban_peer_recipient)
+{
+  using namespace channeler::pipe;
+
+  using peer_policy_t = reject_policy<channeler::peerid_wrapper>;
+  using transport_policy_t = reject_policy<address_t>;
+  using test_filter_t = channeler::pipe::validate_filter<
+    address_t,
+    POOL_BLOCK_SIZE,
+    next,
+    next::input_event,
+    peer_policy_t,
+    transport_policy_t
+  >;
+
+  pool_type pool{PACKET_SIZE};
+
+  // Copy packet data before parsing header
+  auto data = pool.allocate();
+  ::memcpy(data.data(), packet01, sizeof(packet01));
+  data.data()[sizeof(packet01) - 1] = 0x00_b; // Bad checksum - null the last byte
+  channeler::packet_wrapper packet{data.data(), data.size()};
+
+  next n;
+  peer_policy_t t{false};
+  test_filter_t filter{&n, &t};
+
+  // No data added to event.
+  auto ev = std::make_unique<simple_filter_t::input_event>(123, 321, packet, data);
+  auto res = filter.consume(std::move(ev));
+
+  // We expect the event not to be passed on.
+  EXPECT_FALSE(n.m_event);
+
+  // We also expect the result list to contain a recommendation to filter the
+  // source address at the transport level
+  ASSERT_EQ(1, res.size());
+  auto action = *(res.begin());
+  ASSERT_EQ(action->type, AT_FILTER_PEER);
+  auto * ptr = reinterpret_cast<peer_filter_request_action *>(action.get());
+  ASSERT_EQ(ptr->peer, packet.recipient());
+  ASSERT_FALSE(ptr->ingress);
+}
