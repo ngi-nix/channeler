c0260a2f651d084e1ff93d0106d1bcb2336388b6
Jens Finkhaeuser <jens@finkhaeuser.de>
2021-03-02 16:10:32 +0100

    Add & test FSM registry

---
+++ lib/fsm/base.h
@@ -0,0 +1,132 @@
+/**
+ * This file is part of channeler.
+ *
+ * Author(s): Jens Finkhaeuser <jens@finkhaeuser.de>
+ *
+ * Copyright (c) 2021 Jens Finkhaeuser.
+ *
+ * This software is licensed under the terms of the GNU GPLv3 for personal,
+ * educational and non-profit use. For all other uses, alternative license
+ * options are available. Please contact the copyright holder for additional
+ * information, stating your intended usage.
+ *
+ * You can find the full text of the GPLv3 in the COPYING file in this code
+ * distribution.
+ *
+ * This software is distributed on an "AS IS" BASIS, WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.
+ **/
+#ifndef CHANNELER_FSM_BASE_H
+#define CHANNELER_FSM_BASE_H
+
+#ifndef __cplusplus
+#error You are trying to include a C++ only header file
+#endif
+
+#include <channeler.h>
+
+#include "../pipe/event.h"
+#include "../pipe/action.h"
+
+namespace channeler::fsm {
+
+/**
+ * This file defines a base class for finite state machines that handle
+ * parts of the channeler sub-protocols.
+ *
+ * The general interface is similar to the filter interface (see ../pipe),
+ * in that FSMs consume events, whose type is defined in the pipe submodule.
+ *
+ * Unlike filters, each FSM produces actions (to be passed back down the
+ * input filter pipe) as well as events (to be passed to the output filter
+ * pipe). A FSM also does not consume the event it is being passed, but returns
+ * whether or not it processed the event.
+ *
+ * As a result of this, actions and output events are appended to modifiable
+ * list parameters, and only the processing status is returned.
+ *
+ * Also unlike filters, there is no statically compiled pipeline to FSMs.
+ * Rather, each instance may or may not process an event and produce some
+ * results. We therefore need an abstract base class.
+ *
+ * Finally, the input event types are fairly limited. They are invoked at the
+ * end of the input filter pipe, where events are ET_MESSAGE (or in future,
+ * timeouts TODO). This is checked at run-time however.
+ */
+struct fsm_base
+{
+  /**
+   * Process function, see above.
+   */
+  virtual bool process(::channeler::pipe::event * to_process,
+      ::channeler::pipe::action_list_type & result_actions,
+      ::channeler::pipe::event_list_type & output_events) = 0;
+
+  virtual ~fsm_base() = default;
+};
+
+
+/**
+ * A run-time registry of FSMs, to simplify state handling across multiple
+ * FSM implementations.
+ */
+class registry
+{
+public:
+
+  /**
+   * Instanciate and add FSM. There is no removal of FSMs, as it is expected
+   * that the number and composition of FSMs does not change during the run-time
+   * of the protocol. The registry only matters in compositing; for this reason,
+   * we also subsume the construction into the add() function.
+   */
+  template <
+    typename fsmT,
+    typename... ctor_argsT
+  >
+  inline void add(ctor_argsT &&... ctor_args)
+  {
+    static_assert(std::is_base_of<fsm_base, fsmT>::value,
+        "fsmT must be a descendant of fsm_base");
+
+    m_fsms.insert(std::move(
+          std::make_unique<fsmT>(std::forward<ctor_argsT>(ctor_args)...)
+    ));
+  }
+
+
+  /**
+   * Process the input event across all FSMs. Returns true if at least one
+   * registered FSM processed the input event.
+   */
+  inline bool process(::channeler::pipe::event * to_process,
+      ::channeler::pipe::action_list_type & result_actions,
+      ::channeler::pipe::event_list_type & output_events)
+  {
+    if (!to_process) {
+      return false;
+    }
+
+    bool processed = false;
+
+    for (auto & fsm : m_fsms) {
+      if (fsm->process(to_process, result_actions, output_events)) {
+        processed = true;
+      }
+    }
+
+    return processed;
+  }
+
+private:
+  using fsm_ptr = std::unique_ptr<fsm_base>;
+  using fsm_set = std::set<fsm_ptr>;
+
+  fsm_set m_fsms;
+};
+
+
+} // namespace channeler::fsm
+
+#endif // guard
---
+++ test/private/fsm/registry.cpp
@@ -0,0 +1,122 @@
+/**
+ * This file is part of channeler.
+ *
+ * Author(s): Jens Finkhaeuser <jens@finkhaeuser.de>
+ *
+ * Copyright (c) 2021 Jens Finkhaeuser.
+ *
+ * This software is licensed under the terms of the GNU GPLv3 for personal,
+ * educational and non-profit use. For all other uses, alternative license
+ * options are available. Please contact the copyright holder for additional
+ * information, stating your intended usage.
+ *
+ * You can find the full text of the GPLv3 in the COPYING file in this code
+ * distribution.
+ *
+ * This software is distributed on an "AS IS" BASIS, WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.
+ **/
+
+#include "../lib/fsm/base.h"
+
+#include <gtest/gtest.h>
+
+namespace {
+
+struct foo {};
+
+using namespace channeler::pipe;
+
+struct test_fsm : public ::channeler::fsm::fsm_base
+{
+  virtual bool process(event * to_process [[maybe_unused]],
+      action_list_type & result_actions,
+      event_list_type & output_events)
+  {
+    // For the test case, we need to add one action and two
+    // events.
+    result_actions.push_back(std::make_shared<action>());
+
+    output_events.push_back(std::make_shared<event>());
+    output_events.push_back(std::make_shared<event>());
+
+    return true;
+  }
+
+};
+
+} // anonymous namespace
+
+
+TEST(FSMRegistry, add)
+{
+  using namespace channeler::fsm;
+
+  registry reg;
+
+  // Fail at compile time
+  // reg.add<foo>();
+
+  // Succeed!
+  ASSERT_NO_THROW(reg.add<test_fsm>());
+}
+
+
+TEST(FSMRegistry, process_without_fsm)
+{
+  using namespace channeler::pipe;
+  using namespace channeler::fsm;
+
+  registry reg;
+
+  // Without a FSM registered, process() must return false.
+  event ev;
+  action_list_type actions;
+  event_list_type outputs;
+
+  auto res = reg.process(&ev, actions, outputs);
+  ASSERT_FALSE(res);
+  ASSERT_EQ(0, actions.size());
+  ASSERT_EQ(0, outputs.size());
+}
+
+
+TEST(FSMRegistry, process_with_fsm)
+{
+  using namespace channeler::pipe;
+  using namespace channeler::fsm;
+
+  registry reg;
+  EXPECT_NO_THROW(reg.add<test_fsm>());
+
+  // With a FSM registered, process() must produce results
+  event ev;
+  action_list_type actions;
+  event_list_type outputs;
+
+  auto res = reg.process(&ev, actions, outputs);
+  ASSERT_TRUE(res);
+  ASSERT_EQ(1, actions.size());
+  ASSERT_EQ(2, outputs.size());
+}
+
+
+
+TEST(FSMRegistry, process_with_fsm_empty_event)
+{
+  using namespace channeler::pipe;
+  using namespace channeler::fsm;
+
+  registry reg;
+  EXPECT_NO_THROW(reg.add<test_fsm>());
+
+  // With a FSM registered, but no input event, we don't get results.
+  action_list_type actions;
+  event_list_type outputs;
+
+  auto res = reg.process(nullptr, actions, outputs);
+  ASSERT_FALSE(res);
+  ASSERT_EQ(0, actions.size());
+  ASSERT_EQ(0, outputs.size());
+}
