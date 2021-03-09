a6f115178ef29dae778f2b69ef840bd6ae3967be
Jens Finkhaeuser <jens@finkhaeuser.de>
2021-03-09 16:45:40 +0100

    Restructure how channels are held internally; we want to have the initiator part be unique. At the end of the day, the responder part just adds something to the cookie generation process, it seems.

--- lib/channels.h
+++ lib/channels.h
@@ -48,6 +48,21 @@ namespace channeler {
  *
  * For the latter state, a channel instance will be created and maintained
  * as long as the channel is alive.
+ *
+ * As the channelid is defined as having two parts, one chosen by initiator,
+ * the other by responder, each party is tasked with selecting a unique part
+ * for their role. It is technically sufficient for uniquely identifying a
+ * channel.
+ *
+ * Potential for conflict arises when both peers open a channel simultaneously
+ * and choose the same initiator part. In that case, both will log a partial
+ * channelid, and send a MSG_CHANNEL_NEW. When they each receive the other
+ * peer's message, they will find a pending channel. The correct response is
+ * to not respond - this is a fairly rare case, and should be handled by both
+ * sides trying again for new channels.
+ *
+ * But what it means for this class is that we're actually looking up channels
+ * only by the initiator part.
  */
 template <
   typename channelT
@@ -70,13 +85,44 @@ public:
 
   inline bool has_established_channel(channelid const & id) const
   {
-    return m_established.end() != m_established.find(id);
+    auto iter = m_established.find(id.initiator);
+    if (iter == m_established.end()) {
+      // Initiator not found
+      return false;
+    }
+
+    if (iter->second.id.responder != id.responder) {
+      // Initiator is found, but we have a different channelid remembered.
+      // XXX This could actually be a problem.
+      return false;
+    }
+    return true;
   }
 
   inline bool has_pending_channel(channelid const & id) const
   {
-    auto partial = id.create_partial();
-    return m_pending.end() != m_pending.find(partial);
+    // Just consider the initiator part here.
+    return has_pending_channel(id.initiator);
+  }
+
+  inline bool has_pending_channel(channelid::half_type const & initiator) const
+  {
+    // Just consider the initiator part here.
+    return m_pending.end() != m_pending.find(initiator);
+  }
+
+  inline void drop_pending_channel(channelid::half_type const & initiator)
+  {
+    m_pending.erase(initiator);
+  }
+
+  inline channelid get_established_id(channelid::half_type const & initiator) const
+  {
+    auto iter = m_established.find(initiator);
+    if (iter != m_established.end()) {
+      return iter->second.id;
+    }
+    return DEFAULT_CHANNELID;
   }
 
 
@@ -88,22 +134,30 @@ public:
   inline error_t add(channelid const & id)
   {
     if (id == DEFAULT_CHANNELID || id.is_complete()) {
-      auto iter = m_established.find(id);
+      auto iter = m_established.find(id.initiator);
       if (iter != m_established.end()) {
-        // We can ignore this; the channel is already added. No need to error
-        // out, either?
-        return ERR_SUCCESS;
+        if (iter->second.id == id) {
+          // We can ignore this; the channel is already added. No need to error
+          // out, either?
+          return ERR_SUCCESS;
+        }
+        // However, if we currently think that the full channelid is different,
+        // we should produce an error.
+        return ERR_INVALID_CHANNELID;
       }
 
       // TODO lock policy pointer
-      m_established[id] = std::make_shared<channelT>(id, m_packet_size);
+      m_established[id.initiator] = {
+        id,
+        std::make_shared<channelT>(id, m_packet_size),
+      };
       return ERR_SUCCESS;
     }
 
     if (id.has_initiator()) {
       // This is pending, so we can just overwrite existing identifiers. It
       // makes no difference.
-      m_pending.insert(id);
+      m_pending.insert(id.initiator);
       return ERR_SUCCESS;
     }
 
@@ -111,7 +165,6 @@ public:
     // invalid.
     return ERR_INVALID_CHANNELID;
   }
-
 
   /**
    * Update a channel identifier. This is tricky, because it's mostly about
@@ -126,24 +179,26 @@ public:
     }
 
     // If the id is already in the set, we can just ignore the call.
-    auto iter = m_established.find(id);
+    auto iter = m_established.find(id.initiator);
     if (iter != m_established.end()) {
-      return ERR_SUCCESS;
+      if (iter->second.id == id) {
+        return ERR_SUCCESS;
+      }
+      return ERR_INVALID_CHANNELID;
     }
-
-    // Next, we need to construct a new channel identifier from the passed one
-    // that is partial, i.e. has an initiator.
-    auto partial = id.create_partial();
 
     // If this partial identifier is in the pending set, remove it from there
     // and make a full entry.
-    auto iter2 = m_pending.find(partial);
+    auto iter2 = m_pending.find(id.initiator);
     if (iter2 != m_pending.end()) {
       m_pending.erase(iter2);
     }
 
     // TODO lock policy pointer
-    m_established[id] = std::make_shared<channelT>(id, m_packet_size);
+    m_established[id.initiator] = {
+      id,
+      std::make_shared<channelT>(id, m_packet_size),
+    };
 
     return ERR_SUCCESS;
   }
@@ -155,9 +210,11 @@ public:
    */
   inline channel_ptr get(channelid const & id) const
   {
-    auto iter = m_established.find(id);
+    auto iter = m_established.find(id.initiator);
     if (iter != m_established.end()) {
-      return iter->second;
+      if (iter->second.id == id) {
+        return iter->second.data;
+      }
     }
     return {};
   }
@@ -167,12 +224,18 @@ private:
   // TODO: Should we address timeouts of pending channels here? Probably
   //       better in the channel state machine, but that would require
   //       a channel instance.
-  using id_set_t = std::unordered_set<channelid>;
+  using id_set_t = std::unordered_set<channelid::half_type>;
   id_set_t      m_pending;
 
   // For established channels, we also need to keep a set of channel
   // instances.
-  using channel_map_t = std::unordered_map<channelid, channel_ptr>;
+  struct value_type
+  {
+    channelid   id;
+    channel_ptr data;
+  };
+
+  using channel_map_t = std::unordered_map<channelid::half_type, value_type>;
   channel_map_t m_established;
 
   // Packet size
