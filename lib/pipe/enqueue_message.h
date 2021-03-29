2862359fdaa8065a9854ec166ff7f137c6fc86f3
Jens Finkhaeuser <jens@finkhaeuser.de>
2021-03-29 13:48:30 +0200

    WIP: Add filter for taking a message produced by an FSM, and enqueueing it in a channel structure

---
+++ lib/pipe/enqueue_message.h
@@ -0,0 +1,104 @@
+/**
+ * This file is part of channeler.
+ *
+ * Author(s): Jens Finkhaeuser <jens@finkhaeuser.de>
+ *
+ * Copyright (c) 2021 Jens Finkhaeuser.
+ *
+ * This software is licensed under the terms of the GNU GPLv3 for personal,
+ * educational and non-profit use. For all other uses, alternative license
+ * options are available. Please contact the copyright holder for additional
+ * information, stating your intended usage.
+ *
+ * You can find the full text of the GPLv3 in the COPYING file in this code
+ * distribution.
+ *
+ * This software is distributed on an "AS IS" BASIS, WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.
+ **/
+#ifndef CHANNELER_PIPE_ENQUEUE_MESSAGE_H
+#define CHANNELER_PIPE_ENQUEUE_MESSAGE_H
+
+#ifndef __cplusplus
+#error You are trying to include a C++ only header file
+#endif
+
+#include <channeler.h>
+
+#include <memory>
+#include <iostream> // FIXME
+
+#include "../memory/packet_pool.h"
+#include "event.h"
+#include "action.h"
+
+#include <channeler/packet.h>
+#include <channeler/error.h>
+
+
+namespace channeler::pipe {
+
+/**
+ * The enqueue_message filter puts messages into a channel's outgoing
+ * message queue.
+ *
+ * This is simply so that we don't keep pushing messages through the filter
+ * chain. Instead, the buffers should be deciding on which message or packet
+ * to process any given time.
+ */
+template <
+  typename addressT,
+  typename next_filterT,
+  typename next_eventT,
+  typename channelT
+>
+struct enqueue_message_filter
+{
+  using input_event = message_out_event;
+  using channel_set = ::channeler::channels<channelT>;
+
+  inline enqueue_message_filter(next_filterT * next,
+      channel_set & channels)
+    : m_next{next}
+    , m_channels{channels}
+  {
+  }
+
+
+  inline action_list_type consume(std::unique_ptr<event> ev)
+  {
+    if (!ev) {
+      throw exception{ERR_INVALID_REFERENCE};
+    }
+    if (ev->type != ET_MESSAGE_OUT) {
+      throw exception{ERR_INVALID_PIPE_EVENT};
+    }
+
+    input_event * in = reinterpret_cast<input_event *>(ev.get());
+
+    // Get the appropriate channel data
+    auto ch = m_channels.get(in->channel);
+    if (!ch) {
+      // Uh-oh, error
+      // TODO
+      return {};
+    }
+
+    // Enqueue message
+    ch->enqueue_egress_message(std::move(in->message));
+
+    // Create output event
+    auto out = std::make_unique<message_out_enqueued_event>(in->channel);
+    return m_next->consume(std::move(out));
+  }
+
+
+  next_filterT *  m_next;
+  channel_set &   m_channels;
+};
+
+
+} // namespace channeler::pipe
+
+#endif // guard
---
+++ test/private/pipe/enqueue_message.cpp
@@ -0,0 +1,118 @@
+/**
+ * This file is part of channeler.
+ *
+ * Author(s): Jens Finkhaeuser <jens@finkhaeuser.de>
+ *
+ * Copyright (c) 2021 Jens Finkhaeuser.
+ *
+ * This software is licensed under the terms of the GNU GPLv3 for personal,
+ * educational and non-profit use. For all other uses, alternative license
+ * options are available. Please contact the copyright holder for additional
+ * information, stating your intended usage.
+ *
+ * You can find the full text of the GPLv3 in the COPYING file in this code
+ * distribution.
+ *
+ * This software is distributed on an "AS IS" BASIS, WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.
+ **/
+
+#include "../lib/pipe/enqueue_message.h"
+#include "../lib/channel_data.h"
+
+#include <gtest/gtest.h>
+
+namespace {
+
+constexpr std::size_t PACKET_SIZE = 200;
+constexpr std::size_t TEST_POOL_BLOCK_SIZE = 3;
+
+// For testing
+using address_t = uint16_t;
+
+struct next
+{
+  using input_event = channeler::pipe::message_out_enqueued_event;
+
+  inline channeler::pipe::action_list_type consume(std::unique_ptr<channeler::pipe::event> event)
+  {
+    m_event = std::move(event);
+    return {};
+  }
+  std::unique_ptr<channeler::pipe::event> m_event;
+};
+
+
+using filter_t = channeler::pipe::enqueue_message_filter<
+  address_t,
+  next,
+  next::input_event,
+  ::channeler::channel_data<TEST_POOL_BLOCK_SIZE>
+>;
+
+} // anonymous namespace
+
+
+
+TEST(PipeEnqueueMessageFilter, throw_on_invalid_event)
+{
+  using namespace channeler::pipe;
+
+  next n;
+  filter_t::channel_set chs{PACKET_SIZE};
+  filter_t filter{&n, chs};
+
+  // Create a default event; this should not be handled.
+  auto ev = std::make_unique<event>();
+  ASSERT_THROW(filter.consume(std::move(ev)), ::channeler::exception);
+
+  // The filter should also throw on a null event
+  ASSERT_THROW(filter.consume(nullptr), ::channeler::exception);
+}
+
+
+TEST(PipeEnqueueMessageFilter, enqueue_message)
+{
+  using namespace channeler::pipe;
+
+  next n;
+  filter_t::channel_set chs{PACKET_SIZE};
+  filter_t filter{&n, chs};
+
+  auto channel = channeler::create_new_channelid();
+  channeler::complete_channelid(channel);
+  auto err = chs.add(channel);
+  EXPECT_EQ(channeler::ERR_SUCCESS, err);
+  auto ch = chs.get(channel);
+
+  std::byte buf[130]; // It does not matter what's in this memory, but it
+                      // does matter somewhat that the size of the buffer
+                      // exceeds a length encodable in a single byte.
+  auto msg = channeler::message_data::create(buf, sizeof(buf));
+  auto ev = std::make_unique<message_out_event>(
+      channel,
+      std::move(msg)
+  );
+  ASSERT_FALSE(ch->has_egress_data_pending());
+  auto ret = filter.consume(std::move(ev));
+  ASSERT_TRUE(ch->has_egress_data_pending());
+
+  ASSERT_EQ(0, ret.size());
+
+  // Ensure the message has been moved.
+  ASSERT_TRUE(n.m_event);
+  ASSERT_EQ(n.m_event->type, ET_MESSAGE_OUT_ENQUEUED);
+  next::input_event * ptr = reinterpret_cast<next::input_event *>(n.m_event.get());
+  ASSERT_EQ(channel, ptr->channel);
+
+  auto res = ch->dequeue_egress_message();
+  ASSERT_TRUE(res);
+  ASSERT_EQ(channeler::MSG_DATA, res->type);
+  auto resconv = reinterpret_cast<channeler::message_data *>(res.get());
+  ASSERT_EQ(resconv->payload_size, sizeof(buf));
+
+  for (std::size_t i = 0 ; i < resconv->payload_size ; ++i) {
+    ASSERT_EQ(buf[i], resconv->payload[i]);
+  }
+}
