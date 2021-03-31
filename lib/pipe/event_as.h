822420ef11aed19953a965de4625de4a394067c3
Jens Finkhaeuser <jens@finkhaeuser.de>
2021-03-31 22:15:57 +0200

    Add convenience functionality for ensuring event type in filter pipe that can be used for debug logging, but also for safety in debug builds

---
+++ lib/pipe/event_as.h
@@ -0,0 +1,69 @@
+/**
+ * This file is part of channeler.
+ *
+ * Author(s): Jens Finkhaeuser <jens@finkhaeuser.de>
+ *
+ * Copyright (c) 2021 Jens Finkhaeuser.
+ *
+ * This software is licensed under the terms of the GNU GPLv3 for personal,
+ * educational and non-profit use. For all other uses, alternative license
+ * options are available. Please contact the copyright holder for additional
+ * information, stating your intended usage.
+ *
+ * You can find the full text of the GPLv3 in the COPYING file in this code
+ * distribution.
+ *
+ * This software is distributed on an "AS IS" BASIS, WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.
+ **/
+#ifndef CHANNELER_PIPE_EVENT_AS_H
+#define CHANNELER_PIPE_EVENT_AS_H
+
+#ifndef __cplusplus
+#error You are trying to include a C++ only header file
+#endif
+
+#include <channeler.h>
+
+#if defined(DEBUG) && !defined(NDEBUG)
+#include <iostream>
+#endif // DEBUG
+
+#include "event.h"
+
+namespace channeler::pipe {
+
+/**
+ * Helper structure for asserting that a given filter receives an event of a
+ * given type.
+ *
+ * When DEBUG is defined, this is active code. When NDEBUG is defined, only
+ * the conversion is performed.
+ */
+template <
+  typename eventT
+>
+eventT *
+event_as(char const * caller, event * raw, event_type expected_type)
+{
+#if defined(DEBUG) && !defined(NDEBUG)
+  if (!raw) {
+    std::cerr << caller << " received empty event." << std::endl;
+    throw exception{ERR_INVALID_REFERENCE};
+  }
+  if (raw->type != expected_type) {
+    std::cerr << caller << " received unexpected event type: " << raw->type
+      << " (wanted: " << expected_type << ")"
+      << std::endl;
+    throw exception{ERR_INVALID_PIPE_EVENT};
+  }
+  std::cout << caller << " received expected event type: " << raw->type << std::endl;
+#endif // DEBUG
+  return reinterpret_cast<eventT *>(raw);
+}
+
+
+} // namespace channeler::pipe
+
+#endif // guard
